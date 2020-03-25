89154842ef7bbf96473a43ed49b4f91890100f7e
unknown <stubasa.number5@gmail.com>
2020-03-25 23:16:22 +0700

    ducmanhok

--- Project Demo/Form1.Designer.cs
+++ Project Demo/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(567, 22);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "NGUYEN DUC MANH DA VAO DC :3";
+            this.textBox1.Text = "NGUYEN DUC MANH DA VAO DC :3 hihi";
             // 
             // Form1
             // 
